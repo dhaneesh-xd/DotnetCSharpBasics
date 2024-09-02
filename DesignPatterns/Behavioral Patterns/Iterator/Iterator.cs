bb0e6814ba863212ebe311a12c72431a0ada3aa6
dhaneesh-xd <dhaneeshkumarofficial@gmail.com>
2024-09-02 17:20:58 +0530

    update

---
+++ DesignPatterns/Behavioral Patterns/Iterator/Iterator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Behavioral_Patterns.Iterator
+{
+    public interface IAddNumber<D>
+    {
+        bool checkNumber();
+        D Add();
+    }
+    public class AddNumber<D>
+    {
+        public List<D> numbers = new List<D>();
+        public void AddNum(D num)
+        {
+            numbers.Add(num);
+        }
+        public IAddNumber<D> CreateAddFunction()
+        {
+            IAddNumber<D> addNumber = new Addition(this);
+            return addNumber;
+        }
+        private class Addition : IAddNumber<D>
+        {
+            private AddNumber<D> addNumber;
+            private int num = 0;
+            public Addition(AddNumber<D> addNumber)
+            {
+                this.addNumber = addNumber;
+            }
+            public D Add() 
+            {
+                D numD = addNumber.numbers[num++];
+                return numD;
+            }
+            
+            public bool checkNumber()
+            {
+                if (num < addNumber.numbers.Count)
+                    return true;
+                else 
+                    return false;
+            }
+        }
+    }
+}
